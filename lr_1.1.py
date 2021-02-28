08f332edbcbd12528856016134769b5484874631
Анна Парулава <Ap554156@yandex.ru>
2021-02-28 19:51:02 +0300

    Revert "Finush"
    This reverts commit 877d91ed476f2d37af39c268efe7c65b4d6d44c8.

--- lr_1.1.py
+++ lr_1.1.py
@@ -1,21 +1,5 @@
-
-#print("Hello Python from Visual Studio!")
-#s = "*"*30
-#print(s)
-#print("New project")
-#print(s)  
-
-
-import hashlib
-
-dict = {11111: 'Pink', 22222: 'Gray', 33333: 'Petrov', 44444: '553', 55555: 'Anna'}
-word = input("Bведите кодовое слово: ")
-if word in dict.values():  # Есть ли введенное слово в словаре
-    for key, value in dict.items():  # dict.items() - возвращает пары (ключ, значение)
-        if value == word:  # Возвращают список всех значений, доступных в данном словаре k
-            print("Номер ячейки:", key)
-            hash = hashlib.md5(word.encode())  # Возвращает кодированную версию строки
-            hash_res = hash.hexdigest()  # Получить зашифрованную последовательность байтов и строку
-            print("Хеш кодового слова:", hash_res)
-else:
-    print("Кодовое слово введено неверно.")
+print("Hello Python from Visual Studio!")
+s = "*"*30
+print(s)
+print("New project")
+print(s)
